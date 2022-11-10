f92a4ffdc2c4c3ed66df9086c752191b660f77c7
Soya Miyoshi <soyamiyoshi@gmail.com>
2022-11-10 18:23:38 +0900

    zshrc作成

--- .vimrc
+++ .vimrc
@@ -1,1 +1,11 @@
 inoremap <silent> jj <ESC>
+set number
+
+"-------------
+" Change the tab
+"-------------
+"Go to next tab
+nmap <C-Tab> :tabnext<CR>
+
+"Go to previous tab
+nmap <C-S-Tab> :tabprevious<CR>
--- setup.sh
+++ setup.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-DOT_FILES=`ls .* | grep -E '^\..*' | grep -v '^.git:$' | grep -v '.swp$' | grep -v 'gitignore$'| sed -e 's/:/\//g' | grep -v '^\.\/$' | grep -v '^\.\.\/$'`
+DOT_FILES=`ls .* | grep -E '^\..*' | grep -v '^.git:$' | grep -v '.swp$' | grep -v 'gitignore$'| sed -e 's/://g' | grep -v '^\.$' | grep -v '^\.\.$'`
 echo $DOT_FILES
 
 for file in ${DOT_FILES[@]}
 do
-    echo 'Link ${HOME}/${files} to ${HOME}/dotfiles/${file}'
     ln -s $HOME/dotfiles/$file $HOME/$file
 done
 
