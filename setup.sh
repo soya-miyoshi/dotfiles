72c8eb381fcca5a3f5a6f84f4531c0c6a5a1e525
Soya Miyoshi <soyamiyoshi@gmail.com>
2022-11-11 16:52:25 +0900

    zprezto削除

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git clone --recursive https://github.com/sorin-ionescu/prezto.git "$HOME/dotfiles/.zprezto"
+# git clone --recursive https://github.com/sorin-ionescu/prezto.git "$HOME/dotfiles/.zprezto"
 
 DOT_FILES=`ls .* | grep -E '^\..*' | grep -v '^.git:$' | grep -v '.swp$' | grep -v 'gitignore$'| sed -e 's/://g' | grep -v '^\.$' | grep -v '^\.\.$'`
 echo $DOT_FILES
@@ -10,8 +10,8 @@ do
     ln -s $HOME/dotfiles/$file $HOME/$file
 done
 
-for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md; do
-  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
-done
+# for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md; do
+#   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+# done
 
 
