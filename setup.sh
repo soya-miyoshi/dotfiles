eecb4a79b3a9b51176ca16b3cc0460e822216e9f
Soya Miyoshi <soyamiyoshi@gmail.com>
2022-11-11 15:39:11 +0900

    z関係をlinkする位置変更

--- setup.sh
+++ setup.sh
@@ -2,10 +2,6 @@
 
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "$HOME/dotfiles/.zprezto"
 
-for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md; do
-  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
-done
-
 DOT_FILES=`ls .* | grep -E '^\..*' | grep -v '^.git:$' | grep -v '.swp$' | grep -v 'gitignore$'| sed -e 's/://g' | grep -v '^\.$' | grep -v '^\.\.$'`
 echo $DOT_FILES
 
@@ -13,4 +9,9 @@ for file in ${DOT_FILES[@]}
 do
     ln -s $HOME/dotfiles/$file $HOME/$file
 done
+
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md; do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+
 
