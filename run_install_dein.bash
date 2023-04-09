84e28dd0774689914da29c73d01b45795c916a7c
Soya Miyoshi <soyamiyoshi@gmail.com>
2023-04-09 17:54:11 +0900

    chezmoi apply で dein の install まで

---
+++ run_install_dein.bash
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -x
+# shellcheck source=./scripts/common.bash
+source "xdg_helper.bash"
+
+if [ -d "$XDG_DATA_HOME/dein/repos/github.com/Shougo/dein.vim" ]; then
+    echo "dein.vim is already installed."
+    git -C "$XDG_DATA_HOME/dein/repos/github.com/Shougo/dein.vim" pull
+
+    echo "Updating dein.vim plugins..."
+    nvim \
+        -c ":call dein#update()" \
+        -c ":call clap#installer#download_binary()" \
+        -c ":q"
+else
+    echo "Installing dein.vim..."
+    bash install_dein.sh
+
+    echo "Installing dein.vim plugins..."
+    nvim \
+        -c ":call clap#installer#download_binary()" \
+        -c ":q"
+fi
